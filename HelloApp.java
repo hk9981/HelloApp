6501399133aedc32ee6e0d203eceb085aead99cd
hk9981 <hk9981@srmist.edu.in>
2026-03-17 09:20:22 +0530

    UC5: Use enhanced for-loop for multiple names

--- HelloApp.java
+++ HelloApp.java
@@ -1,20 +1,20 @@
 /**
  * HelloApp.java
- * UC4: Display "Hello" with Multiple Command-Line Arguments or Default Message
+ * UC5: Display "Hello" using Enhanced For-Loop with Multiple Arguments or Default Message
  *
  * @author hk9981
  */
 public class HelloApp {
     public static void main(String[] args) {
 
-        // If no arguments, use default
+        // If no arguments → default
         if (args.length == 0) {
             System.out.println("Hello, World!");
         } 
         else {
-            // Loop through all names
-            for (int i = 0; i < args.length; i++) {
-                System.out.println("Hello, " + args[i] + "!");
+            // Enhanced for-loop
+            for (String name : args) {
+                System.out.println("Hello, " + name + "!");
             }
         }
 
